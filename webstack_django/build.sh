381d2ee84f6e6a09e53061393d84af2154182bc7
mBentaifour <bentaifourmoh@gmail.com>
2025-01-16 06:23:18 +0100

    update build script and vercel config

--- webstack_django/build.sh
+++ webstack_django/build.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Upgrade pip
-/opt/conda/bin/python /opt/conda/bin/pip install --upgrade pip
-
-# Install requirements
-/opt/conda/bin/python /opt/conda/bin/pip install -r requirements.txt
-
-# Collect static files
-/opt/conda/bin/python manage.py collectstatic --noinput
-
-# Run migrations
-/opt/conda/bin/python manage.py migrate --noinput
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
