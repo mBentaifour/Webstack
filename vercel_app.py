eafd9f4d8159d4036df200d65a4cadd950e5954f
mBentaifour <bentaifourmoh@gmail.com>
2025-01-18 14:57:08 +0100

    vercel

--- vercel_app.py
+++ vercel_app.py
@@ -1,9 +1,14 @@
 import os
 import sys
+from pathlib import Path
 
 # Add the project root to the Python path
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(path, 'webstack_django'))
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webstack_django.settings')
 
